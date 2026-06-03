063451b8e9953ad90d9135548095eb8050b058d7
Chikezirim Orioha <oriohac@gmail.com>
2026-06-03 16:16:12 +0100

    fix: render cloudinary issues

--- backend/middleware/uploadpostimage.js
+++ backend/middleware/uploadpostimage.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 
 const cloudinary = require("../config/cloudinary");
-const { CloudinaryStorage,} = require("multer-storage-cloudinary");
+const { CloudinaryStorage,} = require("multer-storage-cloudinary").CloudinaryStorage;
 
 // const path = require("path");
 
