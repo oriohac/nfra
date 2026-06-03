fa2b4beaf6b4c638f6501e0df4b7277f3796f72e
Chikezirim Orioha <oriohac@gmail.com>
2026-06-03 01:41:27 +0100

    prepare app for frontend hosting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
-import 'react-quill/dist/quill.snow.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
